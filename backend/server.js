bac3d6c12e6272dd53533453cb492b1c293e37d8
Vibinchandar <vibinmarutharajavp@gmail.com>
2026-01-21 21:20:49 +0530

    updated the server code

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+app.use("/api/order", require("./routes/orderRoutes"));
+
+app.listen(5000, () => console.log("Server Started"));
